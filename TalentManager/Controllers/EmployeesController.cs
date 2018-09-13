45abc3e3a607f9ab9b46002986d2b4e47af02f92
klim kitaev <klim.kitaev@gmail.com>
2018-09-13 20:37:04 +0300

    Chaching

---
+++ TalentManager/Controllers/EmployeesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TalentManager.Models;
+
+namespace TalentManager.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public Employee Get(int id)
+        {
+            return new Employee()
+            {
+                Id = id,
+                Name = "John Q Law",
+                Department = "Enforcement"
+            };
+        }
+        public HttpResponseMessage GetAllEmployees()
+        {
+            var employees = new Employee[]
+            {
+                new Employee()
+                {
+                Id = 12345,
+                Name = "John Q Law",
+                Department = "Enforcement"
+                },
+                new Employee()
+                {
+                Id = 45678,
+                Name = "Jane Q Taxpayer",
+                Department = "Revenue"
+                }
+            };
+
+            var response = Request.CreateResponse<IEnumerable<Employee>>(HttpStatusCode.OK, employees);
+
+            response.Headers.CacheControl = new System.Net.Http.Headers.CacheControlHeaderValue
+            {
+                MaxAge = TimeSpan.FromSeconds(6),
+                MustRevalidate = true,
+                Private = true
+            };
+
+            return response;
+        }
+    }
+}
---
+++ TalentManager/Infrastructure/TimeShareAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace TalentManager.Infrastructure
+{
+    public class TimeShareAttribute : AuthorizeAttribute
+    {
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            IPrincipal principal = Thread.CurrentPrincipal;
+            // If principal.IsInRole("TimeSharer")
+            // Check if current time is between allocated slot start and end times
+            // If not, return false
+            return true;
+        }
+
+    }
+}
